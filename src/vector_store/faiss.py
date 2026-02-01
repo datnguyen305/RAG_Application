6dd6c7833b1a461ec495f5e529660e1c866c2881
datcoding <dat3052005@gmail.com>
2026-02-01 19:29:31 +0700

    add vector store

--- src/vector_store/faiss.py
+++ src/vector_store/faiss.py
@@ -15,22 +15,7 @@ class FaissVectorStore:
             index_to_docstore_id={}
         )
     
-    def add_texts(self, texts, metadatas=None, ids=None):
-        return self.vector_store.add_texts(
-            texts=texts,
-            metadatas=metadatas,
-            ids=ids
-        )
+    def add_documents(self, all_splits):
+        ids = self.vector_store.add_documents(documents=all_splits)
+        return ids
     
-    def similarity_search(self, query, k=4):
-        return self.vector_store.similarity_search(query, k=k)
-
-    def save(self, path):
-        self.vector_store.save_local(path)
-
-    def load(self, path):
-        self.vector_store = FAISS.load_local(
-            path,
-            self.embeddings,
-            allow_dangerous_deserialization=True
-        )
