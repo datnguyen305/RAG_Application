4febbc048fcadd9c72c59b17bf9f06fb299bdd63
datcoding <dat3052005@gmail.com>
2026-02-01 19:02:57 +0700

    embedding

--- src/embeddings/embedding.py
+++ src/embeddings/embedding.py
@@ -1,4 +1,18 @@
+import os
+import config.setting
 from langchain_openai import OpenAIEmbeddings
 
-def get_embedding():
-    return OpenAIEmbeddings(model="text-embedding-3-small")
+
+class Embedded:
+    def __init__(self, model_env: str = "EMBEDDING_MODEL"):
+        self.model_name = os.getenv(model_env)
+        if not self.model_name:
+            raise ValueError(f"Environment variable {model_env} is not set")
+        
+        if self.model_name: 
+            self.model = OpenAIEmbeddings(model=self.model_name)
+    def __repr__(self):
+        return f"Embedded(model_name='{self.model_name}')"
+    
+    def embed(self, texts: list[str]):
+        return self.model.embed_documents(texts)
